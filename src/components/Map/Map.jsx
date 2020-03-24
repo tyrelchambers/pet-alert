71821f26a9d9cea5ca33313e0fb71a77374d6678
Tyrel Chambers <tychambers3@gmail.com>
2020-03-24 17:56:48 -0400

    gogle maps integrate

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,16 +5,62 @@ const google = window.google
 
 const Map = () => {
   const [location, setLocation] = useState({
-    lat: "",
-    long: ""
+    lat: 0,
+    long: 0
   })
 
   const [machine, setMachine] = useState("")
   
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success, error);
-    setMachine("loading")
   }, []);
+
+  useEffect(() => {
+    const currentPos = new google.maps.LatLng({lat: location.lat, lng: location.long});
+    const map = new google.maps.Map(document.getElementById('map'), {center: currentPos, zoom: 15});
+    const infowindow = new google.maps.InfoWindow();
+    const service = new google.maps.places.PlacesService(map);
+
+    const request = {
+      radius: 100,
+      query: "Veterinarian",
+      location: currentPos
+
+    };
+
+    service.textSearch(request, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+
+        map.setCenter(results[0].geometry.location);
+      }
+    });
+
+    function createMarker(place) {      
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
+    
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(`
+          <div class="info-window">
+            <p>${place.name}</p>
+            
+            <div class="flex flex-row items-center map-navigate-wrapper mt-2">
+              <i class="fas fa-external-link-square-alt mr-2 map-navigate"></i>
+              <a class="map-navigate" href="https://www.google.com/maps/dir/?api=1&destination=${place.formatted_address}&dir_action=navigate" target="_blank" rel="noreferrer noopener">Navigate</a>
+            </div>
+          </div>
+        `);
+        infowindow.open(map, this);
+      });
+    }
+
+  }, [location])
+
 
   const success = (pos) => {
     setLocation({
@@ -28,23 +74,11 @@ const Map = () => {
     window.alert('No worky')
   }
   
-  if (machine === "loaded") {
-    return (
-      <div className="map-wrapper">
-        <div id="map">{console.log(window.google)}</div>
-      </div>
-    );
-  }
-
-  if (machine === "loading") {
-    return (
-      <div className="bg">
-        Retrieving location, please wait...
-      </div>
-    )
-  }
-
-  return null;
+  return(
+    <div className="map-wrapper">
+      <div id="map"></div>
+    </div>
+  )
 }
 
 export default Map;
