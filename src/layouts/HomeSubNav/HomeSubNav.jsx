3cc7e11a098ae9a20220fe57f54f97d7217c39f9
Tyrel Chambers <tychambers3@gmail.com>
2020-03-31 09:57:04 -0400

    add search button

--- src/layouts/HomeSubNav/HomeSubNav.jsx
+++ src/layouts/HomeSubNav/HomeSubNav.jsx
@@ -5,12 +5,12 @@ import './HomeSubNav.css'
 const HomeSubNav = () => {
   return (
     <nav className="flex flex-col dash-nav" >
-      <NavLink to="/dashboard" className="dash-link flex flex-row items-center">
+      <NavLink to="/dashboard" className="dash-link flex flex-row items-center" activeClassName="dash-link-active">
         <div className="nav-dot bg-gray-200"></div>
         <i className="fas fa-home mr-2"></i>
         Home
       </NavLink>
-      <NavLink to="/contacts" className="dash-link flex flex-row items-center">
+      <NavLink to="/contacts" className="dash-link flex flex-row items-center" activeClassName="dash-link-active">
         <div className="nav-dot bg-gray-200"></div>
         <i className="fas fa-id-card mr-2"></i>
         Contacts
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Display from '../../layouts/Display/Display';
 import './Search.css'
+import { MainButton } from '../../components/buttons/buttons';
 
 const Search = () => {
   return (
     <Display>
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center w-full">
         <h1 className="title-lg">Search</h1>
-        <input type="text" className="input search" placeholder="enter serial number..." />
+        <div className="flex flex-row">
+          <input type="text" className="input search mr-2" placeholder="enter serial number..." />
+          <MainButton
+            icon={<i className="fas fa-search mr-2"></i>}
+            text="Search"
+            classes="max-w-xs"
+          />
+        </div>
       </div>
     </Display>
   );
